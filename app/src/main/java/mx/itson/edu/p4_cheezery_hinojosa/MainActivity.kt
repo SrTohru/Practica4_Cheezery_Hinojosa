4db8d364db8746092c9ca034bab6e4d540d93545
Jose Hinojosa <74486457+SrTohru@users.noreply.github.com>
2024-06-25 20:35:18 -0700

    Some fixes

--- app/src/main/java/mx/itson/edu/p4_cheezery_hinojosa/MainActivity.kt
+++ app/src/main/java/mx/itson/edu/p4_cheezery_hinojosa/MainActivity.kt
@@ -1,6 +1,8 @@
 package mx.itson.edu.p4_cheezery_hinojosa
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button: Button = findViewById(R.id.button_get_started)
+        button.setOnClickListener {
+            var intent: Intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
